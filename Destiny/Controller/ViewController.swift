e5f8f64a7bb749f964c29813a50795e940c3e104
DmitriiKhristenko <bergamot2142@gmail.com>
2022-08-01 19:46:50 +0300

    добавлен код по MVC паттерну

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    let story0 = "You see a fork in the road."
+    let choice1: String = ""
+    let choice2: String = ""
+    // let font = UIFont(name: "Marker Felt", size: 35) ?? .systemFont(ofSize: 35)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        //choice1Button.titleLabel!.font = UIFont.systemFont(ofSize: 30)
+        
     }
-
-
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getFirstChoice(), for: .normal)
+        choice2Button.setTitle(storyBrain.getSecondChoice(), for: .normal)
+    }
 }
 
